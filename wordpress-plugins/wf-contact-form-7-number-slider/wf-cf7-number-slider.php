8103e0d6a3c1b301c0bf0a7559db61eb5e98cccc
jassiewang2012 <JassieWang@Jassies-iMac.local>
2018-06-20 10:30:47 -0500

    Plugin inital

---
+++ wordpress-plugins/wf-contact-form-7-number-slider/wf-cf7-number-slider.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Plugin Name: Webbed Feet Contact Form 7 Number Slider
+Plugin URI:
+Description: This plugin is an add-on plugin for Contact Form 7. It adds a special number slider filed for the form so you can add price range, age range and more to your contact form. Improve user experience.
+Version:1.0
+Author: Webbed Feet
+Author URI: http://webbedfeetpro.com/
+License: GPLv2
+
+*/
+
+?>
